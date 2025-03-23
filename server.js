c477471f242878ed1625e6d54052ea9850ca1eb4
Rishabh Kumar <rishavkumarsingh707@gmail.com>
2025-03-24 00:01:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,14 +3,8 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = 3000;
 
-mongoose.connect("mongodb+srv://Rishabh123:R1shabh2002@cluster0.lgoyv.mongodb.net/backend?retryWrites=true&w=majority&appName=Cluster0", { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log("DB connected successfully....");
 
-        // Start the server only after the DB connection is successful
-        app.listen(PORT, (err) => {
-            if (err) {
-                console.log(err);
+        
             }
             console.log("Server is running on PORT : " + PORT);
         });
